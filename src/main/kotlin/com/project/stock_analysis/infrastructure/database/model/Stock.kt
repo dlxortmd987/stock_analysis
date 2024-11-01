6eb52ff583f773c20c091ffcf4a7072ee694b19c
Taek Seung Lee <dlxortmd987@gmail.com>
2024-11-01 19:50:33 +0900

    docs: 고민 사항 정리 및 주식 모델 생성

---
+++ src/main/kotlin/com/project/stock_analysis/infrastructure/database/model/Stock.kt
@@ -0,0 +1,16 @@
+package com.project.stock_analysis.infrastructure.database.model
+
+import java.time.LocalDateTime
+
+data class Stock(
+    val id: Long,
+    val name: String,
+    val englishName: String,
+    val standardCode: String,
+    val listingDay: LocalDateTime,
+    val market: String,
+    val stockType: String,
+    val sector: String,
+    val country: String,
+    ) {
+}
