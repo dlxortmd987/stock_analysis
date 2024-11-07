66d563bc5b68c5b92ba683f4c54ab282a77e0be2
Taek Seung Lee <dlxortmd987@gmail.com>
2024-11-07 23:39:10 +0900

    feat: feign 연동 wip

--- src/main/kotlin/com/project/stock_analysis/infrastructure/feign/OpenAiFeignClient.kt
+++ src/main/kotlin/com/project/stock_analysis/infrastructure/feign/OpenAiFeignClient.kt
@@ -2,6 +2,6 @@ package com.project.stock_analysis.infrastructure.feign
 
 import org.springframework.cloud.openfeign.FeignClient
 
-@FeignClient(name = "${feign.openai.name}", url = "${feign.openai.url}")
+@FeignClient(name = "\${feign.openai.name}", url = "\${feign.openai.url}")
 interface OpenAiFeignClient {
 }
