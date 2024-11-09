1b7566af1034aa5c31de8f7b504f95266d694f54
Taek Seung Lee <dlxortmd987@gmail.com>
2024-11-09 11:30:00 +0900

    feat: feign 연동 wip

--- src/main/kotlin/com/project/stock_analysis/infrastructure/feign/FeignConfig.kt
+++ src/main/kotlin/com/project/stock_analysis/infrastructure/feign/FeignConfig.kt
@@ -2,6 +2,7 @@ package com.project.stock_analysis.infrastructure.feign
 
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.PostMapping
 
 @Configuration
 @EnableFeignClients("com.project.stock_analysis.infrastructure.feign")
